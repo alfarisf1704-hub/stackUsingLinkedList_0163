cdb756aa914df8a949d13e5e92487ed3df97f25b
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-15 14:48:16 +0700

    menambahkan node current di else

--- stack.cpp
+++ stack.cpp
@@ -56,6 +56,12 @@ public:
         }
         else
         {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
         }
     }
 };
