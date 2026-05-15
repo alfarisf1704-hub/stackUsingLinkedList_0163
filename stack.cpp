9013570b9c183f7423f18dc4b0b1e0d864d9734a
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-15 15:10:20 +0700

    terakhir menambahkan return 0

--- stack.cpp
+++ stack.cpp
@@ -87,30 +87,32 @@ int main()
         cout << "4. Exit\n";
         cout << "Enter your choice : ";
         cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+
+        case 2:
+            stack.pop();
+            break;
+
+        case 3:
+            stack.peek();
+            break;
+
+        case 4:
+            cout << "Exiting Program." << endl;
+            break;
+
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+        cout << endl;
     }
-
-    switch (choice)
-    {
-    case 1:
-        cout << "Enter the value to push: ";
-        cin >> value;
-        stack.push(value);
-        break;
-
-    case 2:
-        stack.pop();
-        break;
-
-    case 3:
-        stack.peek();
-        break;
-
-    case 4:
-        cout << "Exiting Program." << endl;
-        break;
-
-    default:
-        cout << "Invalid choice. Try again." << endl;
-        break;
-    }
+    return 0;
 }
